726ca3f3ac0fe65559e0f9f0152242cd4e6fe7f0
Pedro Consuegra <pedroconsuegra@Pedros-iMac.local>
2018-09-11 06:20:12 -0500

    nuevas variables

--- lamp.sh
+++ lamp.sh
@@ -19,10 +19,12 @@ apache_modules_install=('modsecurity-2.9.2')
 mysql='mariadb-10.3.7'
 mysql_location='/usr/local/mysql'
 mysql_data_location='/usr/local/mysql/data'
-mysql_root_pass=$password_root
 php='php-7.2.7'
 php_modules_install=('imagick-3.4.3','gmagick-2.0.5RC1')
 phpmyadmin='phpMyAdmin-4.8.2-all-languages'
+mariadb_location='/usr/local/mariadb'
+mariadb_data_location='mariadb_data_location'
+mariadb_root_pass=$password_root
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
